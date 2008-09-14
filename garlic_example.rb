09abe3172f673f36cd2870a562f33e91c9c014d4
Ian White <ian.w.white@gmail.com>
2008-09-14 13:11:36 +1000

    Updated garlic_example - we're only supporting 2.1.x and edge now

--- garlic_example.rb
+++ garlic_example.rb
@@ -23,40 +23,15 @@ garlic do
   repo 'response_for', :path => '.'
 
   # for target, default repo is 'rails', default branch is 'master'
-  target 'edge' do
-    prepare do
-      plugin 'response_for', :clone => true # so we can work on it and push fixes upstream
-    end
-  end
+  target 'edge'
+  target '2.1-stable', :branch => 'origin/2-1-stable'
+  target '2.1.1', :tag => 'v2.1.1'
   
-  target '2.1-RC1' do
-    prepare do
-      plugin 'response_for', :clone => true
-    end
-  end
-  
-  target '2.0-stable', :branch => 'origin/2-0-stable' do
-    prepare do
-      plugin 'response_for', :branch => 'origin/2.0-stable', :clone => true
-    end
-  end
-    
-  target '2.0.2', :tag => 'v2.0.2' do
-    prepare do
-      plugin 'response_for', :branch => 'origin/2.0-stable', :clone => true
-    end
-  end
-  
-  target '2.0.3', :tag => 'v2.0.3' do
-    prepare do
-      plugin 'response_for', :branch => 'origin/2.0-stable', :clone => true
-    end
-  end
-
   all_targets do
     prepare do
       plugin 'rspec'
       plugin('rspec-rails') { sh "script/generate rspec -f" }
+      plugin 'response_for', :clone => true
     end
   
     run do
